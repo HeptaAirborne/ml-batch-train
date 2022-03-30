4859e488e28c79807e2179b7060dec4b9e1148ea
Kuido Kondike <kuido.kondike@heptaairborne.com>
2022-03-30 10:06:14 +0300

    Indent fix

--- train.py
+++ train.py
@@ -9,14 +9,13 @@ from sklearn.model_selection import train_test_split
 from utils.yolov5_utils import train, create_dataset
 from utils.aws_utils import download_images, split_bucket_and_key, get_input_df, read_json_from_s3, upload_file_to_s3
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--aws_id", type=str, default="", help="aws id")
     parser.add_argument("--aws_secret", type=str, default="", help="aws secret")
-    
+
     opt = parser.parse_args()
-    
+
     session = Session(aws_access_key_id=opt.aws_id,
                       aws_secret_access_key=opt.aws_secret)
     s3 = session.resource('s3', verify=False, use_ssl=False)
@@ -45,7 +44,7 @@ if __name__ == "__main__":
     image_ids = unique_df.image_id.values
     imglist_bytes = download_images(image_list, image_ids, opt.aws_id, opt.aws_secret)
     print('Images downloaded.')
-    
+
     print("Creating datasets...")
     train_data, val_data = train_test_split(np.unique(annotations['image_id']), test_size=TEST_SET_SIZE)
     print(f'Train set size: {len(train_data)}, test set size: {len(val_data)}')
@@ -56,19 +55,20 @@ if __name__ == "__main__":
 
     print("Training model...")
     try:
-      train(MODEL_TYPE, MODEL_NAME, CLASSES, IMG_SIZE, BACTH_SIZE, EPOCHS)
+        train(MODEL_TYPE, MODEL_NAME, CLASSES, IMG_SIZE, BACTH_SIZE, EPOCHS)
     except Exception as e:
-      print("Error occured: {e}")
+        print("Error occured: {e}")
     print("Training finished.")
 
     print("Saving model weights to S3...")
-    upload_file_to_s3(s3, 'hepta-ml-model-weights', '', f'./runs/train/{MODEL_NAME}/weights/best.pt', MODEL_NAME + '.pt')
+    upload_file_to_s3(s3, 'hepta-ml-model-weights', '', f'./runs/train/{MODEL_NAME}/weights/best.pt',
+                      MODEL_NAME + '.pt')
 
     print("Adding model setting to models_config.json")
-    models_config[MODEL_NAME] = {'classes' : data['classes']}
+    models_config[MODEL_NAME] = {'classes': data['classes']}
 
     with open('models_config.json', 'w') as f:
-      json.dump(models_config, f)
+        json.dump(models_config, f)
 
     upload_file_to_s3(s3, 'hepta-ml-model-weights', 'config/', './models_config.json', 'models_config.json')
     print("Job completed!")
